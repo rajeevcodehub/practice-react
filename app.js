49fed55a5012832cdecff9859796c40c49e27a97
I527245 <rajeev.kumar13@sap.com>
2024-01-30 22:19:34 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id:"heading2", xyz:"cewe"}, "hello Mr Rajeev BRo");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
